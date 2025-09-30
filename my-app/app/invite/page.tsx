8a2fcae0f35b741ea765b0200e7fe5f063e2603f
minsuk <alstjr0183@naver.com>
2025-09-30 15:24:18 +0900

    작업

--- my-app/app/invite/page.tsx
+++ my-app/app/invite/page.tsx
@@ -14,7 +14,7 @@ export const metadata = {
     "따뜻한 향 가득한 작은 로스터리, 슬기 아빠의 사이커피에 놀러오세요~ ☕️🐾",
   openGraph: {
     title: "슬기 아빠의 사이커피 초대장 ☕️",
-    description: "편하게 들러 한 잔 하고 가세요! 많이 놀러오세요~ 🐶",
+    description: "편하게 들러 한 잔 하고 가세요~🐶",
     images: [
       {
         url: "/슬기og.jpeg", // 새로 준비한 1200x630 파일 권장
